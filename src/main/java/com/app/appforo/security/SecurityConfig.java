d349ac1ad1101b02adcf08c3e58c5971dc8c99cf
brayanvilla555 <brayanalexander098@gmail.com>
2024-06-11 00:38:33 -0500

    Activamos el jwtAuthenticationFilter creado por nosotros

--- src/main/java/com/app/appforo/security/SecurityConfig.java
+++ src/main/java/com/app/appforo/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.app.appforo.security;
 
 
+import com.app.appforo.controller.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,11 +18,15 @@ import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(config -> config.disable())
                 .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(confing -> confing.anyRequest().permitAll())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                //.authorizeHttpRequests(confing -> confing.anyRequest().permitAll())
                 .build();
     }
 
